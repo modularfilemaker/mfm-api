5fd3548a09a39e5a4ed6004ee091502afe5255da
Lui de la Parra <Lui@mutesymphony.com>
2018-09-09 10:27:52 -0700

    Revises Test Strategy
    This commit revises the test strategy. Eventually code coverage should be added.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- test/devops.test.js
+++ test/devops.test.js
@@ -1,12 +1,20 @@
-import assert from "assert";
+/* global describe it */
 
-describe("Devops Route", () => {
-  it("responds with 'pong'", () => {
-    return rp({
-      url: "/devops/ping"
-    }).then(r => {
-      console.log(r);
-      assert.deepEqual(r, "pong");
-    });
-  });
+import { expect } from "chai";
+import { app } from "../src/app";
+import request from "supertest";
+
+describe("Server Information API", () => {
+  describe("Uptime Endpoint", () =>
+    it("responds with a message", done => {
+      request(app)
+        .get("/devops/ping")
+        .end((err, res) => {
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.be.an("object");
+          expect(res.text).to.be.a("string");
+          if (err) return done(err);
+          done();
+        });
+    }));
 });
