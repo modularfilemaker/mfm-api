58d0f2571f7e2edeea22cfab5b5817f93abeeac1
Lui de la Parra <Lui@mutesymphony.com>
2018-09-09 09:53:35 -0700

    Adds Error Handling
    This commit adds error handling to the publish api.
    
    Signed-off-by: Lui de la Parra <Lui@mutesymphony.com>

--- src/api/publish/publish.controller.js
+++ src/api/publish/publish.controller.js
@@ -1,8 +1,9 @@
 import { Filemaker } from "fms-api-client";
 
 const create = (req, res) =>
-  Filemaker.findOne().then(client =>
-    client.script("PublishAPI", "DBTransactions", req.body)
-  );
+	Filemaker.findOne()
+		.then(client => client.script("PublishAPI", "DBTransactions", req.body))
+		.then(response => res.status(200).json(response))
+		.catch(error => res.boom.BadRequest(error.message));
 
 export { create };
